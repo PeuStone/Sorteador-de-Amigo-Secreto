f9490c824e59de1b28ab606d2e5d1df7668af7da
PeuStone <pedrohmotaaa@gmail.com>
2023-09-11 11:04:03 -0300

    Metodo de adicionar na lista funcional

--- src/componentes/Formulario/Formulario.test.tsx
+++ src/componentes/Formulario/Formulario.test.tsx
@@ -1,22 +1,45 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import React from "react";
 import Formulario from "./Formulario";
+import { RecoilRoot } from "recoil";
 
-// Jest
 
 test('quando o input está vazio, novos participantes não podem ser adicionados', () => {
 
     render(<Formulario />)
-
     // encontrar no DOM o input
     const input = screen.getByPlaceholderText('Insira os nomes dos participantes')
+    // encontrar o botão
+    const botao = screen.getByRole('button')
+    // garantir que o input esteja no documento
+    expect(input).toBeInTheDocument()
+    // garantir que o botão esteja desabilitado
+    expect(botao).toBeDisabled()
+})
 
+test('adicionar um participante caso exista um nome preenchido', () => {
+    render(
+        <RecoilRoot>
+            <Formulario />
+        </RecoilRoot>)
+    // encontrar no DOM o input
+    const input = screen.getByPlaceholderText('Insira os nomes dos participantes')
     // encontrar o botão
     const botao = screen.getByRole('button')
 
-    // garantir que o input esteja no documento
-    expect(input).toBeInTheDocument()
+    // inserir um valor no input
+    fireEvent.change(input, {
+        target: {
+            value: 'Ana Catarina'
+        }
+    })
 
-    // garantir que o botão esteja desabilitado
-    expect(botao).toBeDisabled()
+    //clicar no botão de submeter
+    fireEvent.click(botao)
+
+    //garantir que o input esteja com o foco ativo
+    expect(input).toHaveFocus()
+
+    //garantir que o inpur não tenha um valor
+    expect(input).toHaveValue("")
 })
