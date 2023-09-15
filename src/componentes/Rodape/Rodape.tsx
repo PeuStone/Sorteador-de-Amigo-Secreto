9433109d32d66c1901a41b1e35fd7a1f1306ba92
PeuStone <pedrohmotaaa@gmail.com>
2023-09-15 08:38:54 -0300

    Corrigindo Rodape e atualizando snapshot

--- src/componentes/Rodape/Rodape.tsx
+++ src/componentes/Rodape/Rodape.tsx
@@ -18,6 +18,7 @@ const Rodape = () => {
     return (
         <footer className="rodape">
             <button className="botao" disabled={participantes.length < 3} onClick={iniciar}>Iniciar brincadeira</button>
+            <img src="/imagens/sacolas.png" alt="Sacolas de compras" />
         </footer>
     )
 }
